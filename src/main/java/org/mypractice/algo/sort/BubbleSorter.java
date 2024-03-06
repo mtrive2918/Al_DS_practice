5aa02f31ed45226a8c296e1b8da4fd2585cb9470
Maulik Trivedi <33382270+mtrive2918@users.noreply.github.com>
2024-03-06 17:19:48 -0500

    updated bubble sort impl

--- src/main/java/org/mypractice/algo/sort/BubbleSorter.java
+++ src/main/java/org/mypractice/algo/sort/BubbleSorter.java
@@ -9,14 +9,19 @@ public class BubbleSorter<T extends Comparable<T>> extends AbstractTwoWaySorter<
     protected List<T> sort(final List<T> unsortedList) {
         final List<T> mutableList = new ArrayList<>(unsortedList);
         final int len = mutableList.size();
-        for (int i = 0; i < len; i++) {
-            for (int j = len - 1; j >= i; j--) {
-                if (j > 0 && mutableList.get(j - 1).compareTo(mutableList.get(j)) > 0) {
+        int sortedLimit = -1;
+        boolean swapped = true;
+        while (swapped) {
+            swapped = false;
+            for (int j = len - 2; j > sortedLimit; j--) {
+                if (mutableList.get(j).compareTo(mutableList.get(j + 1)) > 0) {
                     T temp = mutableList.get(j);
-                    mutableList.set(j, mutableList.get(j - 1));
-                    mutableList.set(j - 1, temp);
+                    mutableList.set(j, mutableList.get(j + 1));
+                    mutableList.set(j + 1, temp);
+                    swapped = true;
                 }
             }
+            sortedLimit++;
         }
         return mutableList;
     }
